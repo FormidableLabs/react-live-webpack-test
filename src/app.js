1eed7da3e9137174a779f4f8ade240e7778c0fd1
Carlos Kelly <me@carlos.dev>
2023-06-10 15:46:44 -0500

    Added base code

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { LiveProvider, LiveEditor, LivePreview, LiveError } from "react-live"
+
+const code = `
+function handleClick() {
+  console.log("Hi there!");
+}
+const HelloWorld = () => (
+  <button onClick={handleClick}>
+    Hello World
+  </button>
+);
+render(<HelloWorld />);
+`.trim()
+
+
+export const App = () => (
+  <div>
+    <LiveProvider code={code} noInline>
+      <div className="grid lg:grid-cols-2 gap-4">
+        <LiveEditor className="font-mono" />
+        <LivePreview />
+      </div>
+      <LiveError className="text-red-800 bg-red-100 mt-2" />
+    </LiveProvider>
+  </div>
+)
